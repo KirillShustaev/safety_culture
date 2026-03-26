a5cabcb815e04b69489daa74cd381f45d96c1d25
Кирилл Шустаев <kirill.shustaev@yandex.ru>
2026-03-26 14:38:13 +0300

    all

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './styles/index.css';
+
+const buttons = document.querySelectorAll('.info__choice');
+const blocks = document.querySelectorAll('.info__mainInfo');
+
+buttons.forEach(button => {
+    const targetBlock = button.getAttribute('data-target');
+
+
+    button.addEventListener('click', () => {
+        blocks.forEach(block => {
+            if (block.classList.contains(targetBlock)) {
+                block.classList.add("visible");
+                block.classList.remove("none");
+            } else {
+                block.classList.add("none");
+                block.classList.remove("visible");
+            }
+            });
+
+
+
+    });
+});
